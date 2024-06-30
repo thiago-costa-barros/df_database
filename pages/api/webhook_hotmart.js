0b8b9d5311852716460df4dae823baf7f0db2832
Seu Nome <seu@email.com>
2024-06-30 09:35:52 -0300

    feat first migration db

--- pages/api/webhook_hotmart.js
+++ pages/api/webhook_hotmart.js
@@ -1,5 +1,7 @@
 import dotenv from 'dotenv';
+import { PrismaClient } from '@prisma/client';
 
+const prisma = new PrismaClient();
 dotenv.config();
 
 export default async function handler(req, res) {
@@ -13,7 +15,8 @@ export default async function handler(req, res) {
       const MY_RECEIVED_TOKEN = process.env.MY_RECEIVED_TOKEN;
   
       console.log('Tokens recebidos:', { token, receivedToken }); // Log de debug
-  
+      
+      
       if (token !== MY_SECRET_TOKEN || receivedToken !== MY_RECEIVED_TOKEN) {
         console.log('Tokens inválidos, acesso negado'); // Log de debug
         return res.status(403).json({ message: 'Forbidden' });
@@ -21,8 +24,19 @@ export default async function handler(req, res) {
       
       try {
         const data = req.body;
-        
+
         console.log('Dados recebidos do webhook:', data); // Log de debug
+        console.log('Comprador:', data.data.buyer);
+
+        const newUser = await prisma.testUser.create({
+          data: {
+            name: data.data.buyer.name,
+            email: data.data.buyer.email,
+          },
+        });
+  
+        console.log('Novo usuário criado:', newUser);
+
         res.status(200).json({ message: 'Webhook received successfully' });
       } catch (error) {
         console.error('Erro ao processar o webhook:', error);
