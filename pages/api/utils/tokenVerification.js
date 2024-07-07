4fb13596ac97c274fba18ce90164d3f4910a7e35
Seu Nome <seu@email.com>
2024-07-07 12:12:05 -0300

    fix: remove console.log() debug msg

--- pages/api/utils/tokenVerification.js
+++ pages/api/utils/tokenVerification.js
@@ -5,6 +5,5 @@ dotenv.config();
 export const verifyToken = (req) => {
     const token = process.env.MY_SECRET_TOKEN;
     const receivedToken = req.headers['x-hotmart-hottok'];
-    console.log('Tokens recebidos:', { token, receivedToken }); // Log de debug
     return token === receivedToken;
   };
