a1271a328dce3c95d70fc52d7a27bd96f6816d51
Seu Nome <seu@email.com>
2024-07-08 18:37:08 -0300

    fix: message error log

--- pages/api/webhook_hotmart.js
+++ pages/api/webhook_hotmart.js
@@ -43,7 +43,7 @@ export default async function handler(req, res) {
 
       res.status(200).json({ message: 'Webhook received successfully' });
     } catch (error) {
-      console.error('Erro ao processar o webhook:', error);
+      console.error('Error processing webhook:', error);
       res.status(500).json({ message: 'Internal Server Error' });
     }
   } else {
