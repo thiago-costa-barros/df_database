37bb21d04e6821a089cd0aad6515d3804e62f980
Seu Nome <seu@email.com>
2024-07-08 18:51:39 -0300

    add services to endpoints

---
+++ pages/api/webhook_hotmart_events/handleEvents.js
@@ -0,0 +1,11 @@
+"use-server"
+
+import prisma from "/app/_lib/prisma";
+
+export const handleEventsOrder = async (newExternalWebhookHotmartReceiverData) => {
+    try {
+        const eventsOrderData = await {}
+    } catch (error) {
+        
+    }
+}
