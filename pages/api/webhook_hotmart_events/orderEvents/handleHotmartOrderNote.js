59c042cc4b441b1612aa9ba143c1455e6d7441ba
Seu Nome <seu@email.com>
2024-07-09 23:41:23 -0300

    feat: add handleHotmartOrderNote

--- pages/api/webhook_hotmart_events/orderEvents/handleHotmartOrderNote.js
+++ pages/api/webhook_hotmart_events/orderEvents/handleHotmartOrderNote.js
@@ -7,37 +7,21 @@ export async function handleHotmartOrderNote(eventData) {
         const existingHotmartOrderNoteRequestId = await prisma.handleHotmartOrderNote.findFirst({
             where: {
                 requestId: eventData.requestId,
+                deletionDate: null,
             },
         });
         if (!existingHotmartOrderNoteRequestId) {
             // Verifique se já existe um registro com o mesmo hotmartPurchaseTransactionId em HotmartOrderNote
             const existingHotmartOrderNotePurchaseTransactionId = await prisma.hotmartOrderNote.findFirst({
-              where: {
-                hotmartPurchaseTransactionId: eventData.payload.purchase?.transaction,
-              },
-            });
-        if(!existingHotmartOrderNotePurchaseTransactionId){
-            const externalWebhookHotmartReceiverJoin = await prisma.externalWebhookHotmartReceiver.findUnique({
                 where: {
-                    requestId: eventData.requestId,
-                    deletionDate: null,
-                },
-                include: {
-                    HotmartOrderNote:true
+                    hotmartPurchaseTransactionId: eventData.payload.purchase?.transaction,
                 },
             });
-            if(!externalWebhookHotmartReceiverJoin){
-                console.log("No externalWebhookHotmartReceiverJoin found for requestId: ", eventData.requestId);
-                return;
-            }
-            else {  
-                const insertHotmartOrderNote = await prisma.hotmartOrderNote.create({
-
-                })
-            }
+            if (!existingHotmartOrderNotePurchaseTransactionId) {
+                //await insertHotmartOrderEvent(eventData);
             }
         }
     } catch (error) {
-        
+
     }
 }
