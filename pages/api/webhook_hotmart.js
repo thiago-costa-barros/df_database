41a586ef8468427b5f4275195d713600ae825493
Seu Nome <seu@email.com>
2024-07-09 23:40:52 -0300

    feat: add eventRoute with externalWebhookData

--- pages/api/webhook_hotmart.js
+++ pages/api/webhook_hotmart.js
@@ -44,7 +44,7 @@ export default async function handler(req, res) {
 
       res.status(200).json({ message: 'Webhook received successfully' });
 
-      await routeEventHotmartWebhook(data.event, data.data);
+      await routeEventHotmartWebhook(newExternalWebhookHotmartReceiver.eventName, newExternalWebhookHotmartReceiver);
     } catch (error) {
       console.error('Error processing webhook:', error);
       res.status(500).json({ message: 'Internal Server Error' });
