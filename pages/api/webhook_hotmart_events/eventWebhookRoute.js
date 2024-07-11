9c31e37965f4a3769e65659a00e01c8ac031af9a
Seu Nome <seu@email.com>
2024-07-11 08:07:15 -0300

    feat: add log messages

--- pages/api/webhook_hotmart_events/eventWebhookRoute.js
+++ pages/api/webhook_hotmart_events/eventWebhookRoute.js
@@ -7,20 +7,27 @@ import { handleHotmartSwitchPlanNote } from "./switchPlanEvents/handleHotmartSwi
 import { handleHotmartUpdateSubscriptionDateChargeNote } from "./updateSubscriptionDateChargeEvents/handleHotmartUpdateSubscriptionDateChargeNote";
 
 export async function routeEventHotmartWebhook(eventName, eventData){
+    console.log(`Routing event: ${eventName}`);
+    
     switch (eventName) {
         case 'PURCHASE_OUT_OF_SHOPPING_CART':
+            console.log(`Handling event: ${eventName}`);
             await handleHotmartOutOfCheckoutNote(eventData);
             break;
         case 'SUBSCRIPTION_CANCELLATION':
+            console.log(`Handling event: ${eventName}`);
             await handleHotmartSubscriptionCancelationNote(eventData);
             break;
         case 'SWITCH_PLAN':
+            console.log(`Handling event: ${eventName}`);
             await handleHotmartSwitchPlanNote(eventData);
             break;
         case 'UPDATE_SUBSCRIPTION_CHARGE_DATE':
+            console.log(`Handling event: ${eventName}`);
             await handleHotmartUpdateSubscriptionDateChargeNote(eventData);
             break;
         default:
+            console.log(`Handling event: ${eventName}`);
             await handleHotmartOrderNote(eventData);
             break;
     }
